170f8f76ebef14f1b08e6b3a06c32683a731616d
chinweokwu <paulcmorah@gmail.com>
2020-10-19 18:18:05 +0100

    added contact form

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,20 @@
+class ContactsController < ApplicationController
+    def new
+        @contact = Contact.new
+    end
+
+    def index 
+        @contact = Contact.new   
+    end
+    
+    def create
+        @contact = Contact.new(params[:contact])
+        @contact.request = request
+        if @contact.deliver
+            flash.now[:error] = nil
+        else
+            flash.now[:error] = 'Cannot Send Message'
+            render :new
+        end
+    end
+end
